37ed039302b521169e6cdbc8a7c8a8440085e01e
Abdul Moid <abdulmoid7733@gmail.com>
2025-11-14 08:20:27 +0530

    fixes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 
 app.get('/api/hello', (req, res) => {
-res.json({ message: 'Hello from backend! moid', time: new Date().toISOString() });
+res.json({ message: 'Hello from backend! moid'});
 });
 
 
