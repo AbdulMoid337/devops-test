a5b97671f36fc7fd483cd3026f26c01813293104
Abdul Moid <abdulmoid7733@gmail.com>
2025-11-14 07:27:19 +0530

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://devops-test-steel.vercel.app/',
+        target: 'https://devops-test-steel.vercel.app',
         changeOrigin: true,
         secure: false
       }
